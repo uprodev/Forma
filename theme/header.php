df726e8a98e73a749c3033f29aaf144dfd670f24
Stanislav-pr <stanislav.retinskiy@gmail.com>
2024-06-20 13:34:50 +0200

    Update header.php

--- theme/header.php
+++ theme/header.php
@@ -15,7 +15,7 @@
 
 <body <?php body_class() ?>>
 
-    <header<?= is_page_template('templates/contacts.php')?' class="no-anim header-contact"':'';?><?= is_singular('post')?' class="logo-black"':'';?>>
+    <header<?= is_page_template('templates/contacts.php')?' class="no-anim header-contact"':'';?><?= is_singular('post')?' class="logo-black"':'';?><?= is_home()?' class="logo-black no-anim"':'';?>>
         <div class="top-line">
             <div class="content-width" data-aos="fade-down"  data-aos-delay="500" data-aos-duration="1000">
                 <div class="logo-wrap">
